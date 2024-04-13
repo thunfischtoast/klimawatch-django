14aa5541bbe758ee23b7f05b7536f39285076177
thufischtoast <thufischtoast@posteo.de>
2024-04-13 16:38:41 +0200

    youdrawit

--- klimawatch_app/views.py
+++ klimawatch_app/views.py
@@ -32,9 +32,16 @@ def kommune_detail(request, municipality_slug):
     markdown_content = MarkdownContent.objects.filter(kommune=kommune).first()
     markdown_content.content = md.convert(markdown_content.content)
 
-    return HttpResponse(
-        template.render(
-            request=request,
-            context={"kommune": kommune, "markdown_content": markdown_content},
-        )
+    rendered_content = template.render(
+        request=request,
+        context={"kommune": kommune, "markdown_content": markdown_content},
     )
+
+    if "{{ youdrawit }}" in markdown_content.content:
+        youdrawit_template = loader.get_template("youdrawit.html")
+        youdrawit_content = f'<div id=youdrawitroot>{youdrawit_template.render(request=request)}</div>'
+        rendered_content = rendered_content.replace(
+            "{{ youdrawit }}", youdrawit_content
+        )
+
+    return HttpResponse(rendered_content)
