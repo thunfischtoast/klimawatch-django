ed1df79d60f0eab80e1b39c72ac5800f68db01f8
chris <thunfischtoast@posteo.de>
2024-06-30 17:35:23 +0200

    update actions münster

--- scripts/parse_actions.py
+++ scripts/parse_actions.py
@@ -12,9 +12,12 @@ if __name__ == "__main__":
 
         output = {}
 
+        action_count = len(df)
+
         for field, group in df.groupby("Field"):
             # convert group to json and add to output
             output[field] = group.drop(columns=["Field"]).to_dict(orient="records")
 
         with open(f"data/{site}/actions.json", "w", encoding="utf-8") as f:
             json.dump(output, f, indent=4, ensure_ascii=False)
+            print(f"Saved {action_count} actions to data/{site}/actions.json")
