72552314830df420fb87ce7d371af00a5d7c002e
chris <thunfischtoast@posteo.de>
2024-06-30 16:03:43 +0200

    Simpler Progress Indikator

--- klimawatch_app/templatetags/custom_filters.py
+++ klimawatch_app/templatetags/custom_filters.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    print(f"Getting item with key {key} from dictionary {dictionary}")
+    # print(f"Getting item with key {key} from dictionary {dictionary}")
     return dictionary.get(key)
